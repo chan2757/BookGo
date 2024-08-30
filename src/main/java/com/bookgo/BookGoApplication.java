ed3306f8b989e2325ef12a543b1600678758299b
chan <jyc2733@naver.com>
2024-08-30 14:40:33 +0900

    로컬에서 commit 초기화

---
+++ src/main/java/com/bookgo/BookGoApplication.java
@@ -0,0 +1,13 @@
+package com.bookgo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BookGoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BookGoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/bookgo/BookGoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bookgo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BookGoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
