7e4b0af45e52fcfaa95b81a7e68f96538d5ccc60
YellForAll <rizerottes3@gmail.com>
2024-09-09 11:33:25 +0900

    Update BookGoController.java

--- src/main/java/com/bookgo/controller/BookGoController.java
+++ src/main/java/com/bookgo/controller/BookGoController.java
@@ -16,5 +16,30 @@ public class BookGoController {
 		
 		logger.info("메인 페이지 진입");
 	}
+    @GetMapping("/admin")
+    public String adminPage() {
+        logger.info("관리자 페이지 진입");
+        return "admin";
+    }
+
+    @GetMapping("/inquiry")
+    public String inquiryPage() {
+        logger.info("문의 관리 페이지 진입");
+        return "inquiry";
+    }
+
+    @GetMapping("/member")
+    public String memberPage() {
+        logger.info("회원 관리 페이지 진입");
+        return "member";
+    }
+
+    @GetMapping("/inventory")
+    public String inventoryPage() {
+        logger.info("재고 관리 페이지 진입");
+        return "inventory";
+    }
+
+		
 		
 }
